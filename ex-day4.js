42b84b19acc87878789bd83632508a93e76fce4b
natalya <natalya_peixoto@hotmail.com>
2019-09-09 13:57:21 -0300

    extras and links

--- ex-day4.js
+++ ex-day4.js
@@ -10,8 +10,3 @@
 
 // orderando objetos
 //https://medium.com/@renatonogueiraloureno/ordenando-lista-de-objetos-complexos-em-javascript-e373c82d5c36
-
-// Crie uma array com, pelo menos, 4 objetos (com as mesmas propriedades) dentro e
-// depois imprima-os ordenadamente baseado em alguma propriedade
-
-//ex lista de pessoas => imprimir do mais novo para o mais velho.
