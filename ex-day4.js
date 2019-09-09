04985443b566025b7606d8c6a9d0a647fc15e696
natalya <natalya_peixoto@hotmail.com>
2019-09-09 13:51:05 -0300

    editing ex day 5

--- ex-day4.js
+++ ex-day4.js
@@ -1,5 +1,13 @@
 //Objetos complexos
 
+// Com uma função construtora, crie vários objetos que representam pessoas,
+// todos esses objetos devem ter a propriedade 'idade' com valor integer e 'nome' como valor string.
+// para colocar a idade use o Math.random(), use idades entre 15 e 35 anos
+// Insira esses objetos numa array chamada 'pessoas'
+// Crie uma função que ordena por idade essa lista de pessoas
+// imprima a lista do mais novo para o mais velho, da seguinte forma: <nome> tem <idade> anos.
+// Depois imprima apenas a lista contendo maiores de idade.
+
 // orderando objetos
 //https://medium.com/@renatonogueiraloureno/ordenando-lista-de-objetos-complexos-em-javascript-e373c82d5c36
 
--- repro/ex-day5.js
+++ repro/ex-day5.js
@@ -1,3 +1,13 @@
+//vamos popular o objeto abaixo com informações das alunas e professoras.
+const btnEnviarAluna = document.getElementById('enviarAluna')
+const btnEnviarProf = document.getElementById('enviarProf')
+
+// btnEnviarAluna.addEventListener('click', funcão que via ser chamada no evento de click)
+// btnEnviarProf.addEventListener('click', funcão que via ser chamada no evento de click)
+
+const btnListarAlunas = document.getElementById('')
+const btnListarProfs = document.getElementById('')
+
 const reprograma = {
 	alunas: [],
 	professoras: [],
