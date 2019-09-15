a5dd8f37d68d5c41ca18b1a0287bf314e87ec59b
natalya <natalya_peixoto@hotmail.com>
2019-09-15 16:56:29 -0300

    exerc 5

--- repro/ex-day5.js
+++ repro/ex-day5.js
@@ -1,53 +1,39 @@
-const btnEnviarAluna = document.getElementById('enviarAluna')
-const btnEnviarProf = document.getElementById('enviarProf')
-const btnListarAlunas = document.getElementById('listarAlunas')
-const btnListarProfs = document.getElementById('listarProfs')
+// // 1 - escreva o método inserirAluna que vai criar um novo objeto aluna, com propriedades:
+// nome, data de nascimento, mora em são paulo e id. após isso coloque a nova aluna na array alunas.
+
+// // 2- escreva o método listarAlunas, que vai mostrar na tela o nome de todas as alunas que estao na
+// array alunas.
+
+const btnEnviar = document.getElementById('inputAluna')
+const nome = document.getElementById('name')
+const dateOfBirth = document.getElementById('dataDeNascimento')
+const checkbox = document.getElementsByName('nasceuEmSp')
 
 const reprograma = {
 	alunas: [],
-	professoras: [],
 
 	inserirAluna: function(aluna) {},
 
-	inserirProfessora: function() {},
-
 	listarAlunas: function() {},
 
-	listarProfessoras: function() {}
+	listarAlunasDeSP: function() {}
 }
 
-// // 1 - escreva o método inserirAluna que vai salvar o nome de uma aluna(no formato de string)
-// na array alunas.
+function Aluna(nome, date, sp, id) {
+	this.nome = nome
+	this.dateOfBirth = date
+	this.nasceuEmSp = sp
+	this.id = id
+}
 
-// // 2- escreva o método inserirProfessora que vai salvar o nome de uma professora(no formato de string)
-// na array professoras.
-
-// // 3- escreva o método listarAlunas, que vai mostrar na tela o nome de todas as alunas que estao na
-// array alunas.
-
-// // 4- escreva o método listarProfessoras, que vai mostrar na tela o nome de todas as professoras
-// que estão na array professoras.
-
-btnEnviarAluna.addEventListener('click', function(e) {
+let id = 0
+btnEnviar.addEventListener('click', function(e) {
 	e.preventDefault()
-	let inputValue = document.getElementById('inputAluna')
-	reprograma.inserirAluna(inputValue.value)
-	inputValue.value = ''
+	//chamar a função inserirAluna aqui
 })
 
-btnEnviarProf.addEventListener('click', function(e) {
-	e.preventDefault()
-	let inputValue = document.getElementById('inputProf')
-	reprograma.inserirProfessora(inputValue.value)
-	inputValue.value = ''
-})
-
-btnListarAlunas.addEventListener('click', function(e) {
-	e.preventDefault()
-	reprograma.listarAlunas()
-})
-
-btnListarAlunas.addEventListener('click', function(e) {
-	e.preventDefault()
-	reprograma.listarProfessoras()
-})
+function nasceuEmSP() {
+	let nasceu
+	checkbox.forEach(item => (item.checked ? (nasceu = item.value) : ''))
+	return nasceu
+}
